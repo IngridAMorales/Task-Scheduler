915003bc54e021c9d793b200f4a6b53681ef044f
IngridAMorales <68444503+IngridAMorales@users.noreply.github.com>
2021-11-29 23:12:05 -0800

    Update unittest.hpp

--- unittest.hpp
+++ unittest.hpp
@@ -4,16 +4,11 @@
 #include "task.hpp"
 #include "classification.hpp"
 #include "linear.hpp"
-<<<<<<< HEAD
 #include "magnitude.hpp" 
-
-TEST(Linear, TaskList) {
-=======
-#include "magnitude.hpp"
 #include "group.hpp" 
 
 TEST(Magnitude, Display) {
->>>>>>> ccad483aa416509833dcace5a2d33866f33dbf03
+
 	Tasks* test = new Tasks("Cs100", "Software Construction", 1,"School"); 
 	Tasks* task = new Tasks("Final Project", "Task Scheduler", 5,"School");
 	Tasks* subtask1 = new Tasks("Scrum Meeting", "Catch up", 2,"Work"); 
@@ -21,17 +16,11 @@ TEST(Magnitude, Display) {
 	test->add_task(task); 
 	test->add_task(subtask1); 
 	test->add_task(subtask2);
-<<<<<<< HEAD
-	Classification* sort1 = new Linear(); 
+
+	Classification* sort1 = new Magnitude(1,1,3);
 	test->set_strat(sort1);
  	test->printAll(); 
- 
-	EXPECT_EQ(test->printAll(), “Title: Cs100 \n”);
-=======
-	Classification* sort1 = new Magnitude(1,1,3); 
-	test->set_strat(sort1); 
-	test->printAll();
-	
+
 	std::vector<Tasklist*> compare;
 	
 	compare.push_back(subtask1);
@@ -65,7 +54,7 @@ TEST(Group, Display) {
         EXPECT_EQ(test->get_Subtasks(), compare);
 	
 	delete test;
->>>>>>> ccad483aa416509833dcace5a2d33866f33dbf03
+
 
 }
 
