8d03025605df38454a2d3008eb0a282ccad311f0
IngridAMorales <ingridmorales1016@gmail.com>
2021-11-29 20:11:30 -0800

    changes to linear.hpp

--- linear.hpp
+++ linear.hpp
@@ -15,7 +15,7 @@ class Linear : public Classification{
 
         //need a  way to check whether tasks in subtask vector 
         virtual void display(std::vector<Tasklist*> &subtasks ){
-          std::cout<<"Subtasks size: "<< subtasks.size()<<std::endl; 
+          
             for(int i = 0; i<subtasks.size()-1; i++){
                 int min = i;
                 for(int j = i+1; j<subtasks.size(); j++){
@@ -26,11 +26,11 @@ class Linear : public Classification{
                 subtasks[min] = subtasks[i];
                 subtasks[i] = temp;
             }
-	    /*
+	    
             for(int i = 0; i<subtasks.size(); i++){ 
-                subtasks[i]->print(std::cout);
+                subtasks[i]->print();
 	    }
-	   */
+	   
         }
     
 };
